d19bffa68e22f6505a130fc4c859df2eab224608
skywalker <maverick.fengys@gmail.com>
2019-01-11 21:14:36 +0800

    My query builder test framework.

--- src/main/java/com/fys/esclient/ESQueryBuilder.java
+++ src/main/java/com/fys/esclient/ESQueryBuilder.java
@@ -1,6 +1,41 @@
 package com.fys.esclient;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+
 import static org.elasticsearch.index.query.QueryBuilders.*;
 
 public class ESQueryBuilder {
+    public static void main(String[] args) {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")
+                )
+        );
+
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+
+        try {
+            SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+            System.out.println(response.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/com/fys/esclient/ESQueryBuilderTest.java
+++ src/test/java/com/fys/esclient/ESQueryBuilderTest.java
@@ -1,6 +1,44 @@
 package com.fys.esclient;
 
-import org.elasticsearch.test.ESTestCase;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
-public class ESQueryBuilderTest extends ESTestCase {
+import java.io.IOException;
+
+public class ESQueryBuilderTest {
+    private static RestHighLevelClient client;
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before class.");
+        client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")
+                )
+        );
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After class.");
+        try {
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCase1() {
+        System.out.println("Test Case 1");
+    }
+
+    @Test
+    public void testCase2() {
+        System.out.println("Test Case 2");
+    }
 }
