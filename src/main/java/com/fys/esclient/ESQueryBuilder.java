5fbba544a90ca2c39693b069e8d24595d4c76360
skywalker <maverick.fengys@gmail.com>
2019-01-11 17:56:49 +0800

    Create ESQueryBuilder class

---
+++ src/main/java/com/fys/esclient/ESQueryBuilder.java
@@ -0,0 +1,6 @@
+package com.fys.esclient;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+public class ESQueryBuilder {
+}
---
+++ src/test/java/com/fys/esclient/ESQueryBuilderTest.java
@@ -0,0 +1,6 @@
+package com.fys.esclient;
+
+import org.elasticsearch.test.ESTestCase;
+
+public class ESQueryBuilderTest extends ESTestCase {
+}
