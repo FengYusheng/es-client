029bd3a08c9ba74705aeb0f838c74b5dfd9c41e9
skywalker <maverick.fengys@gmail.com>
2019-01-11 22:35:41 +0800

    Rafactor ESQueryBuilder

--- src/main/java/com/fys/esclient/ESQueryBuilder.java
+++ src/main/java/com/fys/esclient/ESQueryBuilder.java
@@ -1,50 +1,20 @@
 package com.fys.esclient;
 
-import org.apache.http.HttpHost;
-import org.apache.lucene.util.QueryBuilder;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.MatchAllQueryBuilder;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
-
-import java.io.IOException;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 public class ESQueryBuilder {
 
-    public static MatchAllQueryBuilder matchAll() {
-        return matchAllQuery();
+    public static SearchSourceBuilder matchAll() {
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(matchAllQuery());
+        sourceBuilder.from(0);
+        sourceBuilder.size(0);
+        return sourceBuilder;
     }
 
     public static void main(String[] args) {
-//        RestHighLevelClient client = new RestHighLevelClient(
-//                RestClient.builder(
-//                        new HttpHost("localhost", 9200, "http")
-//                )
-//        );
-//
-//        SearchRequest searchRequest = new SearchRequest("bank");
-//        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-//        searchSourceBuilder.query(matchAllQuery());
-//        searchSourceBuilder.from(0);
-//        searchSourceBuilder.size(10);
-//        searchRequest.source(searchSourceBuilder);
-//
-//        try {
-//            SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-//            System.out.println(response.toString());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            client.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        System.out.println("ES Query DSL Builder");
     }
 }
--- src/test/java/com/fys/esclient/ESQueryBuilderTest.java
+++ src/test/java/com/fys/esclient/ESQueryBuilderTest.java
@@ -6,8 +6,8 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.*;
 import org.junit.Test;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -18,7 +18,6 @@ import java.io.IOException;
 public class ESQueryBuilderTest {
     private static RestHighLevelClient client;
     private static final String index = "bank";
-    private SearchRequest searchRequest;
 
     @BeforeClass
     public static void beforeClass() {
@@ -41,27 +40,15 @@ public class ESQueryBuilderTest {
     }
 
     @Test
-    public void testCase1() {
-        System.out.println("Test Case 1");
-    }
-
-    @Test
-    public void testCase2() {
-        System.out.println("Test Case 2");
-    }
-
-    @Test
-    public void testMatchAll() {
-        searchRequest = new SearchRequest(index);
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.query(ESQueryBuilder.matchAll());
-        searchSourceBuilder.from(0);
-        searchSourceBuilder.size(0);
-        searchRequest.source(searchSourceBuilder);
+    public void test_match_all() {
+        SearchRequest searchRequest = new SearchRequest(index);
+        SearchSourceBuilder sourceBuilder = ESQueryBuilder.matchAll();
+        searchRequest.source(sourceBuilder);
 
         try {
             SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-            System.out.println(response.toString());
+            assertHitCount(response, 1000);
+            assertNoFailures(response);
         } catch (IOException e) {
             e.printStackTrace();
         }
